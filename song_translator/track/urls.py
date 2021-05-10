8478729c65d980fb04afa4a6363e269e1b9f4dd1
anatoli <anatoli@wsb-167-72a.icx.local>
2021-05-10 14:59:06 +0300

    add check languages in url and add put, delete for translate

--- song_translator/track/urls.py
+++ song_translator/track/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import TrackList, TrackDetail, SingerList, SingerDetail, TrackTranslate
+from .views import TrackList, TrackDetail, SingerList, SingerDetail, TrackTranslate, track_translate
 
-
+lang = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ny', 'co', 'hr', 'cs', 'da', 'nl',
+         'en', 'eo', 'et', 'tl',
+         'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'iw', 'he', 'hi', 'hu', 'is', 'icelandic', 'ga',
+         'irish', 'it'
+     , 'kn', 'kk', 'la', 'lv', 'lt', 'pl', 'pt', 'ru', 'es', 'sv', 'tg', 'tr', 'tk', 'uk', 'uz']
+lang_re = '|'.join(lang)
 urlpatterns = [
     path('tracks/', TrackList.as_view()),
     path('tracks/<int:pk>/', TrackDetail.as_view()),
-    path('tracks/<int:pk>/<str:lang>', TrackTranslate.as_view()),
+    re_path(r'^tracks/(?P<pk>\d+)/(?P<lang>'+lang_re+')/$', track_translate),
     path('singers/', SingerList.as_view()),
     path('singers/<int:pk>/', SingerDetail.as_view()),
 ]
--- song_translator/track/views.py
+++ song_translator/track/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from google_trans_new import google_translator
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 from .models import Track, Singer, Translate
 from .serializers import SingerSerializer, TrackSerializer, TranslateSerializer
@@ -19,11 +22,9 @@ class TrackDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
-class TrackTranslate(generics.ListCreateAPIView):
-    queryset = Translate.objects.all()
-    serializer_class = TranslateSerializer
-
-    def get(self, request, pk, lang):
+@api_view(['GET', 'PUT', 'DELETE'])
+def track_translate(request, pk, lang):
+    try:
         translate = Translate.objects.filter(track_id=pk, language=lang)
         if not translate:
             translator = google_translator()
@@ -31,13 +32,30 @@ class TrackTranslate(generics.ListCreateAPIView):
             track_serializer = TrackSerializer(track)
             Translate.objects.create(track_id=track, language=lang,
                                      text=translator.translate(track_serializer.data['text'],
-                                                                    lang_tgt=lang,
-                                                                    lang_src=track_serializer.data[
-                                                                        'original_language']))
+                                                               lang_tgt=lang,
+                                                               lang_src=track_serializer.data[
+                                                                   'original_language']))
             translate = Translate.objects.filter(track_id=pk, language=lang)
-        serializer = TranslateSerializer(translate.get())
+        translate = translate.get()
+    except Translate.DoesNotExist:
+        return Response({'The translate does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TranslateSerializer(translate)
         data = serializer.data['text']
         return Response(data)
+
+    elif request.method == 'PUT':
+        translate_data = JSONParser().parse(request)
+        translate_serializer = TranslateSerializer(translate, data=translate_data)
+        if translate_serializer.is_valid():
+            translate_serializer.save()
+            return Response(translate_serializer.data)
+        return Response(translate_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        translate.delete()
+        return Response('Tutorial was deleted successfully!', status=status.HTTP_204_NO_CONTENT)
 
 
 class SingerList(generics.ListCreateAPIView):
